86d3e214a1d1569f522c83b2a3c62b2d2f45cb61
mike agee <kb0vaq@yahoo.com>
2018-07-27 21:53:57 -0600

    module 9 wip

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,6 @@
+
+// Register Babel to transpile before our tests are run
+require('babel-register')();
+
+// Disable webpack features that Mocha doesn;t understand
+require.extensions['.css'] = function() {};
